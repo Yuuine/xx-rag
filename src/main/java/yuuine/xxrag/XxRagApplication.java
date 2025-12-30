286929459ebb52cb961c04a46728becd822a1278
yunqing <jzhang0703@163.com>
2025-12-31 01:20:53 +0800

    init

---
+++ src/main/java/yuuine/xxrag/XxRagApplication.java
@@ -0,0 +1,13 @@
+package yuuine.xxrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yuuine/xxrag/XxRagApplicationTests.java
@@ -0,0 +1,13 @@
+package yuuine.xxrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
