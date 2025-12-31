8a717ef72dd89560a596c0a051f3cdae2f147d61
yunqing <jzhang0703@163.com>
2025-12-31 22:19:29 +0800

    refactor(app): 重构应用配置和组件扫描设置

--- src/main/java/yuuine/xxrag/XxRagApplication.java
+++ src/main/java/yuuine/xxrag/XxRagApplication.java
@@ -2,26 +2,20 @@ package yuuine.xxrag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.modulith.ApplicationModule;
+import yuuine.xxrag.ingestion.exception.GlobalExceptionHandler;
 
 @SpringBootApplication
-@ApplicationModule
-@EnableFeignClients(basePackages = "yuuine.xxrag.app.client")
-@ComponentScan(basePackages = {
-        "yuuine.xxrag.app",
-        "yuuine.xxrag.inference",
-        "yuuine.xxrag.ingestion",
-        "yuuine.xxrag.vector"
-}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
-        yuuine.xxrag.ingestion.exception.GlobalExceptionHandler.class
-}))
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = GlobalExceptionHandler.class
+        )
+)
 public class XxRagApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XxRagApplication.class, args);
     }
-
 }
