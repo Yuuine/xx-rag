ab5d08c783e188580d97c844231bb720ff350155
yunqing <jzhang0703@163.com>
2026-01-16 18:40:29 +0800

    feat: 添加 WebSocket 心跳任务

--- src/main/java/yuuine/xxrag/XxRagApplication.java
+++ src/main/java/yuuine/xxrag/XxRagApplication.java
@@ -3,9 +3,11 @@ package yuuine.xxrag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class XxRagApplication {
 
     public static void main(String[] args) {
